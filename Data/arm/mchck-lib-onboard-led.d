aa0f40a17de31d3e58b8f03b7db287279866dd37
itecwip <itecwip@github.com>
2015-12-07 01:30:56 +0100

    update

---
+++ Data/arm/mchck-lib-onboard-led.d
@@ -0,0 +1,179 @@
+mchck-lib-onboard-led.d mchck-lib-onboard-led.o: \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/onboard-led.c \
+ /home/nicolas/dev/mchck/toolchain/include/mchck_internal.h \
+ /home/nicolas/dev/mchck/toolchain/include/mchck.h \
+ /home/nicolas/dev/mchck/toolchain/include/mchck-cdefs.h \
+ /home/nicolas/dev/mchck/toolchain/include/MK20DZ10.h \
+ /home/nicolas/dev/mchck/toolchain/include/intnums.h \
+ /home/nicolas/dev/mchck/toolchain/include/vecs_k20.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/ftfl.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/usbotg.h \
+ /home/nicolas/dev/mchck/toolchain/lib/usb/usb-common.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/sim.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/mcg.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/rcm.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/port.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/gpio.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/pmc.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/adc.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/spi.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/lptmr.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/ftm.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/i2c.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/pit.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/tsi.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/uart.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/rtc.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/crc.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/llwu.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/smc.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/vbat.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/vref.h \
+ /home/nicolas/dev/mchck/toolchain/include/kinetis/cmp.h \
+ /home/nicolas/dev/mchck/toolchain/include/arm/scb.h \
+ /home/nicolas/dev/mchck/toolchain/include/arm/nvic.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/mchck.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/system.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/pin.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/pin_change.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/gpio.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/flash.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/onboard-led.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/adc.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/adc_queue.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/spi.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/sg.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/timeout.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/stdio.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/strtol.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/ftm.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/crc.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/pit.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/uart.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/rtc.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/spiflash.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/i2c.h \
+ /home/nicolas/dev/mchck/toolchain/lib/mchck/i2c_reg.h \
+ /home/nicolas/dev/mchck/toolchain/lib/usb/usb.h \
+ /home/nicolas/dev/mchck/toolchain/lib/usb/dfu.h \
+ /home/nicolas/dev/mchck/toolchain/lib/usb/cdc-acm.h \
+ /home/nicolas/dev/mchck/toolchain/lib/usb/hid.h \
+ /home/nicolas/dev/mchck/toolchain/lib/nrf/nrf.h
+
+/home/nicolas/dev/mchck/toolchain/include/mchck_internal.h:
+
+/home/nicolas/dev/mchck/toolchain/include/mchck.h:
+
+/home/nicolas/dev/mchck/toolchain/include/mchck-cdefs.h:
+
+/home/nicolas/dev/mchck/toolchain/include/MK20DZ10.h:
+
+/home/nicolas/dev/mchck/toolchain/include/intnums.h:
+
+/home/nicolas/dev/mchck/toolchain/include/vecs_k20.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/ftfl.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/usbotg.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/usb/usb-common.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/sim.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/mcg.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/rcm.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/port.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/gpio.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/pmc.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/adc.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/spi.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/lptmr.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/ftm.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/i2c.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/pit.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/tsi.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/uart.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/rtc.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/crc.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/llwu.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/smc.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/vbat.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/vref.h:
+
+/home/nicolas/dev/mchck/toolchain/include/kinetis/cmp.h:
+
+/home/nicolas/dev/mchck/toolchain/include/arm/scb.h:
+
+/home/nicolas/dev/mchck/toolchain/include/arm/nvic.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/mchck.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/system.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/pin.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/pin_change.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/gpio.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/flash.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/onboard-led.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/adc.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/adc_queue.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/spi.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/sg.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/timeout.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/stdio.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/strtol.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/ftm.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/crc.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/pit.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/uart.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/rtc.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/spiflash.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/i2c.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/mchck/i2c_reg.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/usb/usb.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/usb/dfu.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/usb/cdc-acm.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/usb/hid.h:
+
+/home/nicolas/dev/mchck/toolchain/lib/nrf/nrf.h:
